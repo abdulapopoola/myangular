31dbbd239703a0fc824c21d28623739f5e35cbeb
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-06-29 22:27:58 -0700

    DI angular

---
+++ src/loader.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function setupModuleLoader(window) {
+    var ensure = function (obj, name, factory) {
+        return obj[name] || (obj[name] = factory());
+    };
+
+    var angular = ensure(window, 'angular', Object);
+
+    var createModule = function (name, requires, modules) {
+        if (name === 'hasOwnProperty') {
+            throw 'hasOwnProperty is not a valid module name';
+        }
+        var moduleInstance = {
+            name: name,
+            requires: requires
+        };
+        modules[name] = moduleInstance;
+        return moduleInstance;
+    };
+
+    var getModule = function (name, modules) {
+        if (modules.hasOwnProperty(name)) {
+            return modules[name];
+        } else {
+            throw 'Module ' + name + ' is not available!';
+        }
+    };
+
+    ensure(angular, 'module', function () {
+        var modules = {};
+        return function (name, requires) {
+            if (requires) {
+                return createModule(name, requires, modules);
+            } else {
+                return getModule(name, modules);
+            }
+        };
+    });
+}
+
+module.exports = setupModuleLoader;
