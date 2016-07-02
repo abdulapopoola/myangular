54d635e2f486943ba60225b6a90331285e4628d4
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-07-01 22:30:46 -0700

    injector

--- src/injector.js
+++ src/injector.js
@@ -2,6 +2,10 @@
 
 var _ = require('lodash');
 
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
+
 function createInjector(modulesToLoad) {
     var cache = {};
     var loadedModules = {};
@@ -28,13 +32,44 @@ function createInjector(modulesToLoad) {
         }
     });
 
+    function annotate(fn) {
+        if (_.isArray(fn)) {
+            return fn.slice(0, fn.length - 1);
+        } else if (fn.$inject) {
+            return fn.$inject;
+        } else if (!fn.length) {
+            return [];
+        } else {
+            var source = fn.toString().replace(STRIP_COMMENTS, '');
+            var argDeclaration = source.match(FN_ARGS);
+            return _.map(argDeclaration[1].split(','), function (argName) {
+                return argName.match(FN_ARG)[2];
+            });
+        }
+    }
+
+    function invoke(fn, thisObj, locals) {
+        var args = _.map(fn.$inject, function (token) {
+            if (_.isString(token)) {
+                return locals && locals.hasOwnProperty(token) ?
+                    locals[token] :
+                    cache[token];
+            } else {
+                throw 'Incorrect injection token! Expected a string, got ' + token;
+            }
+        });
+        return fn.apply(thisObj, args);
+    }
+
     return {
         has: function (key) {
             return cache.hasOwnProperty(key);
         },
         get: function (key) {
             return cache[key];
-        }
+        },
+        annotate: annotate,
+        invoke: invoke
     };
 }
 
--- test/injector_spec.js
+++ test/injector_spec.js
@@ -83,4 +83,123 @@ describe('injector', function () {
         window.angular.module('myOtherModule', ['myModule']);
         createInjector(['myModule']);
     });
+
+    it('invokes an annotated function with dependency injection', function () {
+        var module = window.angular.module('myModule', []);
+        module.constant('a', 1);
+        module.constant('b', 2);
+        var injector = createInjector(['myModule']);
+
+        var fn = function (one, two) { return one + two; };
+        fn.$inject = ['a', 'b'];
+
+        expect(injector.invoke(fn)).toBe(3);
+    });
+
+    it('does not accept non-strings as injection tokens', function () {
+        var module = window.angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+
+        var fn = function (one, two) { return one + two; };
+        fn.$inject = ['a', 2];
+
+        expect(function () {
+            injector.invoke(fn);
+        }).toThrow();
+    });
+
+    it('invokes a function with the given this context', function () {
+        var module = window.angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+
+        var obj = {
+            two: 2,
+            fn: function (one) { return one + this.two; }
+        };
+        obj.fn.$inject = ['a'];
+
+        expect(injector.invoke(obj.fn, obj)).toBe(3);
+    });
+
+    it('overrides dependencies with locals when invoking', function () {
+        var module = window.angular.module('myModule', []);
+        module.constant('a', 1);
+        module.constant('b', 2);
+        var injector = createInjector(['myModule']);
+
+        var fn = function (one, two) { return one + two; };
+        fn.$inject = ['a', 'b'];
+
+        expect(injector.invoke(fn, undefined, { b: 3 })).toBe(4);
+    });
+
+    describe('annotate', function () {
+        it('returns the $inject annotation of a function when it has one', function () {
+            var injector = createInjector([]);
+
+            var fn = function () { };
+            fn.$inject = ['a', 'b'];
+
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        });
+
+        it('returns the array-style annotations of a function', function () {
+            var injector = createInjector([]);
+
+            var fn = ['a', 'b', function () { }];
+
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        });
+
+        it('returns an empty array for a non-annotated 0-arg function', function () {
+            var injector = createInjector([]);
+
+            var fn = function () { };
+
+            expect(injector.annotate(fn)).toEqual([]);
+        });
+
+        it('returns annotations parsed from function args when not annotated', function () {
+            var injector = createInjector([]);
+
+            var fn = function (a, b) { };
+
+            expect(injector.annotate(fn)).toEqual(['a', 'b']);
+        });
+
+        it('strips comments from argument lists when parsing', function () {
+            var injector = createInjector([]);
+
+            var fn = function (a, /*b,*/ c) { };
+
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        });
+
+        it('strips several comments from argument lists when parsing', function () {
+            var injector = createInjector([]);
+
+            var fn = function (a, /*b,*/ c/*, d*/) { };
+
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        });
+
+        it('strips // comments from argument lists when parsing', function () {
+            var injector = createInjector([]);
+
+            var fn = function (a, //b,
+                c) { };
+
+            expect(injector.annotate(fn)).toEqual(['a', 'c']);
+        });
+
+        it('strips surrounding underscores from argument names when parsing', function () {
+            var injector = createInjector([]);
+
+            var fn = function (a, _b_, c_, _d, an_argument) { };
+            
+            expect(injector.annotate(fn)).toEqual(['a', 'b', 'c_', '_d', 'an_argument']);
+        });
+    });
 });
