1ce49012f969895d28db4a11340ae7a79579edbd
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-07-02 16:14:07 -0700

    providers

--- src/loader.js
+++ src/loader.js
@@ -12,12 +12,19 @@ function setupModuleLoader(window) {
             throw 'hasOwnProperty is not a valid module name';
         }
         var invokeQueue = [];
+
+        var invokeLater = function (method) {
+            return function () {
+                invokeQueue.push([method, arguments]);
+                return moduleInstance;
+            };
+        };
+
         var moduleInstance = {
             name: name,
             requires: requires,
-            constant: function(key, value) {
-                invokeQueue.push(['constant', [key, value]]);
-            },
+            constant: invokeLater('constant'),
+            provider: invokeLater('provider'),
             _invokeQueue: invokeQueue
         };
         modules[name] = moduleInstance;
--- test/injector_spec.js
+++ test/injector_spec.js
@@ -300,9 +300,23 @@ describe('injector', function () {
             function Type(a, b) {
                 this.result = a + b;
             }
-            
+
             var instance = injector.instantiate(Type, { b: 3 });
             expect(instance.result).toBe(4);
+        });
+
+        it('allows registering a provider and uses its $get', function () {
+            var module = window.angular.module('myModule', []);
+            module.provider('a', {
+                $get: function () {
+                    return 42;
+                }
+            });
+
+            var injector = createInjector(['myModule']);
+            
+            expect(injector.has('a')).toBe(true);
+            expect(injector.get('a')).toBe(42);
         });
     });
 });
