1329aeff57efd9963a90aaf5fb5d1548fc651c48
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-07-25 18:09:01 -0700

    http headers

--- src/http.js
+++ src/http.js
@@ -7,31 +7,74 @@ function isSuccess(status) {
 }
 
 function $HttpProvider() {
-    var defaults = {
+    var defaults = this.defaults = {
         headers: {
             common: {
                 Accept: 'application/json, text/plain, */*'
+            },
+            post: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            put: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            patch: {
+                'Content-Type': 'application/json;charset=utf-8'
             }
         }
     };
 
     function mergeHeaders(config) {
-        return _.extend(
+        var reqHeaders = _.extend(
             {},
-            defaults.headers.common,
             config.headers
         );
+        var defHeaders = _.extend(
+            {},
+            defaults.headers.common,
+            defaults.headers[(config.method || 'get').toLowerCase()]
+        );
+        _.forEach(defHeaders, function (value, key) {
+            var headerExists = _.some(reqHeaders, function (v, k) {
+                return k.toLowerCase() === key.toLowerCase();
+            });
+            if (!headerExists) {
+                reqHeaders[key] = value;
+            }
+        });
+        return executeHeaderFns(reqHeaders, config);
+    }
+
+    function executeHeaderFns(headers, config) {
+        return _.transform(headers, function (result, v, k) {
+            if (_.isFunction(v)) {
+                v = v(config);
+                if (_.isNull(v) || _.isUndefined(v)) {
+                    delete result[k];
+                } else {
+                    result[k] = v;
+                }
+            }
+        }, headers);
     }
 
     this.$get = ['$httpBackend', '$q', '$rootScope',
         function ($httpBackend, $q, $rootScope) {
-            return function $http(requestConfig) {
+            function $http(requestConfig) {
                 var deferred = $q.defer();
 
                 var config = _.extend({
                     method: 'GET'
                 }, requestConfig);
                 config.headers = mergeHeaders(requestConfig);
+
+                if (_.isUndefined(config.data)) {
+                    _.forEach(config.headers, function (v, k) {
+                        if (k.toLowerCase() === 'content-type') {
+                            delete config.headers[k];
+                        }
+                    });
+                }
 
                 function done(status, response, statusText) {
                     status = Math.max(status, 0);
@@ -52,7 +95,9 @@ function $HttpProvider() {
                     done,
                     config.headers);
                 return deferred.promise;
-            };
+            }
+            $http.defaults = defaults;
+            return $http;
         }];
 }
 
